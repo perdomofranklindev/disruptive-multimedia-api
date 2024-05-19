c4dc6d3de1d9b50daff3c6ecde8a257c49a2eaa1
perdomofranklindev <perdomofranklindev>
2024-05-19 12:17:46 -0400

    Project setups

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+		project: './tsconfig.json'
+	},
+	plugins: ['@typescript-eslint', 'prettier'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'prettier'
+	],
+	rules: {
+		'prettier/prettier': 'error',
+		'@typescript-eslint/no-floating-promises': 0
+	}
+};
