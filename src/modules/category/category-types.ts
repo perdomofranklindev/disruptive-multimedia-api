6556fc52a976fa34c05c612ac687982b313f9b7c
perdomofranklindev <perdomofranklindev>
2024-06-03 11:30:29 -0400

    Type defined

---
+++ src/modules/category/category-types.ts
@@ -0,0 +1,12 @@
+export enum Category {
+	Text = 'TEXT',
+	Image = 'IMAGE',
+	Picture = 'PICTURE',
+	Url = 'URL',
+	Document = 'DOCUMENT',
+	File = 'FILE',
+	Video = 'VIDEO',
+	YouTubeVideo = 'YOUTUBE_VIDEO'
+}
+
+export type CategoryType = `${Category}`;
---
+++ src/modules/role/role-types.ts
@@ -0,0 +1,7 @@
+export enum Role {
+	Reader = 'READER',
+	Creator = 'CREATOR',
+	Admin = 'ADMIN'
+}
+
+export type RoleType = `${Role}`;
