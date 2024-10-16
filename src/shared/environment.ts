0c0c007cbeec3beaddd43eb18e4901c7d6ff0923
perdomofranklindev <perdomofranklindev>
2024-10-16 16:36:42 -0400

    new variable envs declared

--- src/shared/environment.ts
+++ src/shared/environment.ts
@@ -3,3 +3,5 @@ dotenv.config();
 
 export const PORT = process.env.PORT ?? 3000;
 export const SALT_ROUNDS = Number(process.env.SALT_ROUNDS);
+export const JWT_SECRET = String(process.env.JWT_SECRET);
+export const ENVIRONMENT = process.env.ENVIRONMENT;
